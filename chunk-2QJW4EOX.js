96e109e9e2941c92b1edfd7ea8c362c530b2a57b
mentino <graziano.giuseppe@proton.me>
2026-08-02 15:31:28 +0200

    Auto-generated commit

---
+++ chunk-2QJW4EOX.js
@@ -0,0 +1,1 @@
+import{n as s}from"./chunk-H2FECCZR.js";import{qa as i,wa as a}from"./chunk-IREDKWF5.js";var p=(()=>{let t=class t{constructor(r){this._translateService=r}transform(r){return this._translateService.translate(r)}};t.\u0275fac=function(n){return new(n||t)(i(s,16))},t.\u0275pipe=a({name:"translate",type:t,pure:!0});let e=t;return e})();export{p as a};
